5b143ac825500e2650fd28ed6df91c41f51e8f09
Sean Parkin <sdparkin@protonmail.com>
2023-04-13 23:26:19 -0500

    Created validation file

---
+++ src/utils/validation.ts
@@ -0,0 +1,34 @@
+import { RegisterErrors } from '../../types'
+
+export const checkRegister = (values: any) => {
+  const errors: any = {}
+  if (!values.name) {
+    errors.name = 'Name is required'
+  }
+  if (!values.email) {
+    errors.email = 'Email is required'
+  } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+    errors.email = 'Email is invalid'
+  }
+  if (!values.password) {
+    errors.password = 'Password is required'
+  } else if (values.password.length < 6) {
+    errors.password = 'Password must be at least 6 characters'
+  }
+  if (!values.confirmPassword) {
+    errors.confirmPassword = 'Confirm password is required'
+  }
+  if (!values.lat) {
+    errors.lat = 'Latitude is required'
+  }
+  if (!values.long) {
+    errors.long = 'Longitude is required'
+  }
+  Object.keys(errors).forEach((key) => {
+    if (errors[key as keyof RegisterErrors] === '') {
+      delete errors[key as keyof RegisterErrors]
+    }
+  })
+
+  return errors
+}
