d88c196aad42cf9a6ace34b24c96a64da761f854
Sean Parkin <sdparkin@protonmail.com>
2023-04-24 23:40:03 -0500

    Added User type

--- types/index.d.ts
+++ types/index.d.ts
@@ -16,6 +16,14 @@ export interface RegisterErrors {
   long: string
 }
 
+export interface User {
+  name: string
+  email: string
+  password: string
+  lat: number
+  long: number
+}
+
 export interface Login {
   email: string
   password: string
