c3503e6be939aae564c0fc7c0d2a4bd6cd59ee09
Sean Parkin <sdparkin@protonmail.com>
2023-06-04 22:49:20 -0500

    Created register api auth route

---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from 'next/types'
+import bcrypt from 'bcryptjs'
+import { connectToDatabase } from '../../../utils/mongodb'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { db } = await connectToDatabase()
+  const user = await db.collection('users').findOne({ email: req.body.email })
+
+  if (!user) {
+    return res.status(400).json({ error: 'Email not found' })
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(req.body.password, salt)
+  const createUser = await db.collection('users').insertOne({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword,
+    role: 'user',
+    lat: req.body.lat,
+    long: req.body.long,
+  })
+
+  return res.status(200).json({ success: true, data: createUser })
+}
