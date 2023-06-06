a3e12ad52a1a14ad3690ae99e0f009da67565483
Sean Parkin <sdparkin@protonmail.com>
2023-06-05 21:18:49 -0500

    Fix for build error

--- src/pages/update.tsx
+++ src/pages/update.tsx
@@ -5,7 +5,7 @@ import { sanitize } from 'dompurify'
 import TextInput from '@/components/TextInput'
 import styles from '@/styles/Register.module.css'
 import { checkRegister } from '@/utils/validation'
-import RegisterMap from '../components/RegisterMap'
+import RegisterMap from '../components/RegisterMap/RegisterMap'
 import { Register, RegisterErrors } from '../../types'
 
 // TODO: Finish design
