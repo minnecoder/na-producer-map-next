550aaa62ac898ebe78aee09ece27c012fb1f27f9
Sean Parkin <sdparkin@protonmail.com>
2023-06-10 21:51:24 -0500

    Marker data is passed to Map component

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head'
 import Map from '@/components/Map'
+import { useEffect, useState } from 'react'
 import Header from '@/components/Header'
+import { User } from '../../types'
 
 function MapPage() {
+  const [mapData, setMapData] = useState<User[]>([])
+  useEffect(() => {
+    fetch('/api/user')
+      .then((res) => res.json())
+      .then((data) => {
+        setMapData(data.data)
+      })
+  }, [])
   return (
     <>
       <Head>
         <title>Map</title>
       </Head>
       <Header />
-      <Map />
+      <Map data={mapData} />
     </>
   )
 }
